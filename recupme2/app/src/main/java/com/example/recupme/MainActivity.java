03258d02585364a220fbce329bd7c2485816e076
Paulo André <andre.oliveira.belo@gmail.com>
2021-01-31 23:04:34 -0300

    Ajustes e correção de bugs

--- recupme2/app/src/main/java/com/example/recupme/MainActivity.java
+++ recupme2/app/src/main/java/com/example/recupme/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.recupme;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,37 +19,105 @@ import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private static final String DEBUG_TAG = "https://www.emprestaumcopo.com.br";
+
+
+   // Button btn;
+
+
+    public boolean temInternet() {
+
+        ConnectivityManager manager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        if(info != null && info.isConnected()){
+
+            //verificarConexao.setVisibility(View.INVISIBLE);
+            webView = findViewById(R.id.webView);
+            webView.loadUrl("https://www.emprestaumcopo.com.br");
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+            webView.getSettings().setAllowFileAccessFromFileURLs(true);
+            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            webView.getSettings().setBuiltInZoomControls(true);
+            webView.getSettings().setAllowFileAccess(true);
+            webView.getSettings().setSupportZoom(true);
+        }else{
+            Toast.makeText(MainActivity.this,"Sem conexao",Toast.LENGTH_SHORT).show();
+
+            //Button verificarConexao = (Button) findViewById(R.id.verificarConexao);
+
+          /*  btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    temInternet();
+                    btn.setVisibility(View.INVISIBLE);
+                }
+            });*/
+        }
+        return info != null && info.isConnected();
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if(keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()){
             webView.goBack();
+            webView.getSettings().setAppCacheEnabled(false);
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
 
+    //@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //btn = (Button) findViewById(R.id.verificarConexao);
+        //btn.setVisibility(View.INVISIBLE);
+       /*
+        if(!temInternet()){
+            Toast.makeText(MainActivity.this,"Sem conexao",Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.INVISIBLE);
+            webView.setVisibility(View.INVISIBLE);
+        }else {
+            progressBar.setVisibility(View.VISIBLE);
+            webView.setVisibility(View.VISIBLE);
+        }
+*/
+
         webView = findViewById(R.id.webView);
         webView.loadUrl("https://www.emprestaumcopo.com.br");
         webView.getSettings().setJavaScriptEnabled(true);
-
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setSupportZoom(true);
 
         final ProgressBar progressBar = findViewById(R.id.progress);
         progressBar.setVisibility(View.INVISIBLE);
+
         webView.setWebViewClient(new WebViewClient(){
+
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
                 progressBar.setVisibility(View.VISIBLE);
+
             }
 
             @Override
@@ -52,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
                 super.onPageFinished(view, url);
                 progressBar.setVisibility(View.INVISIBLE);
             }
+
             /*
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
@@ -76,5 +150,7 @@ public class MainActivity extends AppCompatActivity {
             }
             */
         });
+
+
     }
 }
